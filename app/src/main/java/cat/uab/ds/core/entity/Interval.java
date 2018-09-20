7d599798faf4ffd43a5b1f9ebe157a1c7a7978ad
Dani Fargas <danifargas@outlook.com>
2018-09-20 14:14:17 +0200

    Interval Class Fill

--- app/src/main/java/cat/uab/ds/core/entity/Interval.java
+++ app/src/main/java/cat/uab/ds/core/entity/Interval.java
@@ -1,7 +1,54 @@
 package cat.uab.ds.core.entity;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Represents interval of time
  */
 public class Interval {
+
+    private Date start;
+    private Date end;
+
+    public long getDuration(){
+        if(end != null){
+            return end.getTime() - start.getTime();//as given
+        }else{
+            return  0;
+        }
+    }
+
+    public String getDurationStr(){
+        long diff = getDuration();
+
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(diff);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(diff);
+        long hours = TimeUnit.MILLISECONDS.toMinutes(diff);
+        long days = TimeUnit.MILLISECONDS.toMinutes(diff);
+
+        return ((days!=0)?"Days: "+ days:"")
+                + ((hours!=0)?"Hours: "+ hours:"")
+                + ((minutes!=0)?"Minutes: "+ minutes:"")
+                + ((seconds!=0)?"Seconds: "+ seconds:"");
+    }
+
+    @Override
+    public String toString() {
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        String endStr = " - ";
+        String durationStr = "";
+
+        if(end != null){
+            endStr = df.format(end);
+            durationStr = " | " + getDurationStr();
+        }
+
+        return "Start: " + df.format(start) + " End: "+ endStr + durationStr;
+    }
 }
