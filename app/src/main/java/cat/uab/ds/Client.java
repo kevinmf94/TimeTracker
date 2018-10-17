ef0c07ba402bbd3b0ba3e3b5312d0d167e216dc2
Kevin Martin <kevinmf94@gmail.com>
2018-10-17 17:34:02 +0200

    Client changes

--- app/src/main/java/cat/uab/ds/Client.java
+++ app/src/main/java/cat/uab/ds/Client.java
@@ -25,8 +25,8 @@ public class Client {
 
         timeTracker = new TimeTracker();
 
-        /*Project pr1 = new Project("P1");
-        tt.addProject(pr1);
+        Project pr1 = new Project("P1");
+        timeTracker.addProject(pr1);
 
         Task task3 = new TaskBasic("T3");
         pr1.addActivity(task3);
@@ -37,22 +37,7 @@ public class Client {
         Task task1 = new TaskBasic("T1");
         Task task2 = new TaskBasic("T2");
         pr2.addActivity(task1);
-        pr2.addActivity(task2);*/
-
-        //Test Decorator
-        //Task task4 = new TaskBasic("T4");
-        //Date now = new Date();
-        //task4 = new TaskScheduled(task4, new Date(now.getTime()+4000));
-        //task4 = new TaskLimited(task4,4);
-
-        //task4 = TaskScheduled
-        //((TaskDecorator) task4).removeComponent(TaskScheduled.class);
-        //pr2.addActivity(task4);
-        //task4.start();*/
-
-        //Save/Load Test
-        timeTracker.load("data.dat");
-        //tt.save("data.dat");
+        pr2.addActivity(task2);
 
         //Test Secuencial
         /*task3.start();
@@ -82,6 +67,30 @@ public class Client {
         task3.start();
         wait(2000);
         task3.stop();*/
+
+        //Save/Load Test
+        //timeTracker.load("data.dat");
+        //timeTracker.save("data.dat");
+
+        //Test Decorator
+        Date now = new Date();
+        task1 = new TaskScheduled(task1, new Date(now.getTime()+4000));
+        task1 = new TaskLimited(task1, 8);
+
+
+        //Task task4 = new TaskBasic("T4");
+        //Date now = new Date();
+        //task4 = new TaskScheduled(task4, new Date(now.getTime()+4000));
+        //task4 = new TaskLimited(task4,4);
+
+        //task4 = TaskScheduled
+        //((TaskDecorator) task4).removeComponent(TaskScheduled.class);
+        //pr2.addActivity(task4);
+        //task4.start();*/
+
+
+
+
     }
 
     private static void wait(int milis){
